c9ca02b47a112b6d717955c2009ecef370b8bcf9
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-12-20 17:33:36 -0300

    Initial virtual machine implementation

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
-#include "common.h"
 #include "chunk.h"
+#include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char const* argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
   writeConstant(&chunk, 1.2, 123);
   writeChunk(&chunk, OP_RETURN, 123);
-  
+
   disassembleChunk(&chunk, "test chunk");
+
+  interpret(&chunk);
+
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
